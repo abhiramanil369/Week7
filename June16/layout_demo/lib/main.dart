a942451e614eb77b25179f69614f515438e7ade7
abhhiramanil369 <abhiramforcoding369@gmail.com>
2025-06-16 13:22:26 +0530

    layout class created and made changes

--- June16/layout_demo/lib/main.dart
+++ June16/layout_demo/lib/main.dart
@@ -11,25 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      title: 'Flutter Layout Demo',
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -117,6 +99,41 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class LayoutScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    appBar: AppBar(
+      title: const Text('Column Layout Example'),
+    ),
+    body: Column(
+      children: [
+        Container(
+          height: 150,
+        width: double.infinity,
+        color:Colors.red,
+        child: const Center(
+            child: Text(
+            'Top Container',
+            style: TextStyle(color:Colors.white,fontSize: 20),
+            ), 
+          ),
+        ),
+        Container(
+          height:150,
+          width:double.infinity,
+          color: Colors.blue,
+          child: const Center(
+            child: Text(
+              'Bottom Container',
+              style: TextStyle(color:Colors.white,fonstSize:20),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
